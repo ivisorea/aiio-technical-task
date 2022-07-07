7ea46805fa96eb8fbc6ada1c3bcb72fd7809cd7b
Ivis Orea <ivisorea@gmail.com>
2022-07-07 10:02:08 +0200

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('renders learn react link', () => {
+  it('should render App', async() => {
+    // render(<App />);
+    // expect(screen.getByText('Hello World')).toBeInTheDocument();
+  }
+  );
+}
+);
+
