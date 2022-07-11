fefa6a9725fc6ea53580f8c1be0423bc83601aa0
Ivis Orea <ivisorea@gmail.com>
2022-07-11 12:13:39 +0200

    fix: filtering condition

--- src/components/SelectionOverview.jsx
+++ src/components/SelectionOverview.jsx
@@ -6,12 +6,22 @@ export const SelectionOverview = ({selectedData}) => {
   return (
     <>
         <h3>Products</h3>
+        {selectedData.length > 0 
+          ? 
         <p>{selectedData.products.filter(product => product.name).map(product => product.name).join(', ')}</p>
+          : <p>No products selected</p>}
         <h3>Sub categories</h3>
-        <p>{selectedData.subCategories.filter(subCategory => subCategory.name).map(subCategory => subCategory.name).join(', ')}</p>
+        {selectedData.length > 0
+          ?
+          <p>{selectedData.subCategories.filter(subCategory => subCategory.name).map(subCategory => subCategory.name).join(', ')}</p>
+          : <p>No sub categories selected</p>}
         <h3>Sub products</h3>
-        <p>{selectedData.subProducts.filter(subProduct => subProduct.name).map(subProduct => subProduct.name).join(', ')}</p>
+        {selectedData.length > 0
+          ?
+          <p>{selectedData.subProducts.filter(subProduct => subProduct.name).map(subProduct => subProduct.name).join(', ')}</p>
+          : <p>No sub products selected</p>}
         <div style={{display: 'flex', justifyContent: 'flex-end' }}>
+
         <button 
             style={{backgroundColor: 'white', color: '#5774b6', fontWeight: 'bold', border: 'none', marginTop: '1rem', }}
             onClick={() => setOpenModal(false)}>
